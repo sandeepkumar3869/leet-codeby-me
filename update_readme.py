0d18f1d367f0e97e52ae81e5f02eea46754c3db3
sandeepkumar3869 <sksandy3869@gmail.com>
2024-12-24 20:57:36 +0530

    Your commit message

--- update_readme.py
+++ update_readme.py
@@ -19,11 +19,11 @@ def update_readme(stats, repo_path="README.md"):
             contest_rating = stats.get('contest_rating', 'N/A')  # Default to 'N/A' if not present
             readme[i] = f"- **Contest Rating**: {contest_rating}  \n"
         elif "| 🟢 Easy" in line:
-            readme[i] = f"| 🟢 Easy        | {stats.get('easySolved', 0)} / {stats.get('totalEasy', 0)}          | {stats.get('acceptanceRate', '0')}%              | Beats {stats.get('acceptanceRate', '0')}%  |\n"
+            readme[i] = f"| 🟢 Easy        | {stats.get('easySolved', 0)} / {stats.get('totalEasy', 0)}          | {stats.get('acceptanceRate', 0):.2f}%              |\n"
         elif "| 🟡 Medium" in line:
-            readme[i] = f"| 🟡 Medium      | {stats.get('mediumSolved', 0)} / {stats.get('totalMedium', 0)}         | {stats.get('acceptanceRate', '0')}%             | Beats {stats.get('acceptanceRate', '0')}%  |\n"
+            readme[i] = f"| 🟡 Medium      | {stats.get('mediumSolved', 0)} / {stats.get('totalMedium', 0)}         | {stats.get('acceptanceRate', 0):.2f}%             |\n"
         elif "| 🔴 Hard" in line:
-            readme[i] = f"| 🔴 Hard        | {stats.get('hardSolved', 0)} / {stats.get('totalHard', 0)}            | {stats.get('acceptanceRate', '0')}%             | Beats {stats.get('acceptanceRate', '0')}%  |\n"
+            readme[i] = f"| 🔴 Hard        | {stats.get('hardSolved', 0)} / {stats.get('totalHard', 0)}            | {stats.get('acceptanceRate', 0):.2f}%             |\n"
 
     with open(repo_path, "w") as file:
         file.writelines(readme)
@@ -32,3 +32,5 @@ if __name__ == "__main__":
     leetcode_username = "SKSANDY2396"
     stats = fetch_leetcode_stats(leetcode_username)
     update_readme(stats)
+    # print(stats)
+
